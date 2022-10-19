44300ae01d5fe1d971be96ed95e995696b379f5d
kaje94 <a.kajendran@gmail.com>
2022-10-19 21:14:16 +0530

    Update terms and condition page

--- landing/src/components/Footer/Footer.tsx
+++ landing/src/components/Footer/Footer.tsx
@@ -1,14 +1,18 @@
 import { component$ } from "@builder.io/qwik";
 
 export const Footer = component$(() => {
-  return (
-    <footer className="bg-gray-800  w-full">
-      <div className="pt-2">
-        <div className="flex justify-between pb-5 px-3 m-auto pt-5 border-t border-gray-500 text-gray-400 text-sm flex-col md:flex-row max-w-6xl">
-          <div className="mt-2">© 2022 Copyright: Beyojar</div>
-          <div className="mt-2">Terms & Conditions</div>
-        </div>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="bg-gray-800  w-full">
+            <div className="pt-2">
+                <div className="flex justify-between pb-5 px-3 m-auto pt-5 border-t border-gray-500 text-gray-400 text-sm flex-col md:flex-row max-w-6xl">
+                    <a href="/">
+                        <div className="mt-2 hover:text-gray-500 duration-300">© 2022 Copyright: Beyojar</div>
+                    </a>
+                    <a href="/terms_and_conditions">
+                        <div className="mt-2 hover:text-gray-500 duration-300">Terms & Conditions</div>
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
 });
