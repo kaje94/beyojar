f7286b4bf48a79500a1eab51907994f2a9729dfa
kaje94 <a.kajendran@gmail.com>
2022-10-23 17:39:22 +0530

    Fix toast component margin top

--- mobile/src/components/molecules/ToastComponent/ToastComponent.tsx
+++ mobile/src/components/molecules/ToastComponent/ToastComponent.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 
 import { MessageComponentProps } from "react-native-flash-message";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useTheme } from "styled-components";
 
 import { BorderRadius, Spacing } from "@src/common/theme";
@@ -9,6 +10,7 @@ import { FlexBox, Text } from "@src/components/atoms";
 /** Custom toast message to be shown for the showMessage function of react-native-flash-message */
 export const ToastComponent: FC<MessageComponentProps> = ({ message, icon }) => {
     const { pallette } = useTheme();
+    const insets = useSafeAreaInsets();
 
     return (
         // eslint-disable-next-line react-native-a11y/has-accessibility-hint
@@ -18,6 +20,7 @@ export const ToastComponent: FC<MessageComponentProps> = ({ message, icon }) => 
             bg={message.backgroundColor || pallette.primary.main}
             borderBottomLeftRadius={BorderRadius.medium}
             borderBottomRightRadius={BorderRadius.medium}
+            mt={insets.top}
             p={Spacing.medium}
         >
             <>
