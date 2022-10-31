38ecf9f3949592223071971d4dfccc92c0860382
kaje94 <a.kajendran@gmail.com>
2022-10-31 06:22:22 +0530

    Remove sst and add expo to workspaces

--- stacks/index.ts
+++
@@ -1,9 +0,0 @@
-import { DynamoDbStack } from "./dynamoDb";
-import { AppSyncStack } from "./appSync";
-
-import { App } from "@serverless-stack/resources";
-
-export default function (app: App) {
-    app.stack(DynamoDbStack);
-    app.stack(AppSyncStack);
-}
