fe7a74b3011696d271cdec488c3b562bf1314267
kaje94 <a.kajendran@gmail.com>
2022-11-14 21:58:57 +0530

    Update sonar coverage exclusions

--- mobile/src/lang/i18n.ts
+++ mobile/src/lang/i18n.ts
@@ -3,7 +3,7 @@ import { initReactI18next } from "react-i18next";
 
 import en from "./en.json";
 
-// TODO: once there is more than one language file,
+// Once there is more than one language file,
 // Use a language detector and set the language automatically
 // http://i18next.com/docs/ownplugin/#languagedetector
 
